6a3da1c73d7634353899b512a303de8cbaa016d0
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-12 19:44:20 -0400

    updated function

--- src/fsConnect.js
+++ src/fsConnect.js
@@ -2,9 +2,6 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { credentials } from "../credentials.js";
 
-initializeApp({
-  credential: cert(credentials),
-});
 export function dbConnect() {
   if (!getApps().length) {
     initializeApp({
